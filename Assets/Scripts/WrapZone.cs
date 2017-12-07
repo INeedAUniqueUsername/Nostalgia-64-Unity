dca4da5dccbc3ecb76946d1bdb5f54c9567d94ca
INeedAUniqueUsername <alexiscomical445@gmail.com>
2017-12-07 14:31:49 -0800

    Fix WrapZone wraparound bug

--- Assets/Scripts/WrapZone.cs
+++ Assets/Scripts/WrapZone.cs
@@ -30,7 +30,7 @@ public class WrapZone : MonoBehaviour {
 			
 			if(col.transform.position.x - 1 <= transform.position.x  - triggerDimensions.x/2) {
 				col.gameObject.transform.position += new Vector3(triggerDimensions.x, 0);
-			} else if(col.transform.position.x + 1 >= transform.position.x + triggerDimensions.y/2) {
+			} else if(col.transform.position.x + 1 >= transform.position.x + triggerDimensions.x/2) {
 				col.gameObject.transform.position += new Vector3(-triggerDimensions.x, 0);
 			}
 			if(col.transform.position.y - 1 <= transform.position.y - triggerDimensions.y/2) {
